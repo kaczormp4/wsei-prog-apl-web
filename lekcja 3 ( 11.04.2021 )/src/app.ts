1b941c213b476d14aac17476708087bdc9b5cd74
kaczormp4 <kaczkaoryginal@gmail.com>
2021-04-21 22:43:06 +0200

    first progress

--- lekcja 3 ( 11.04.2021 )/src/app.ts
+++ lekcja 3 ( 11.04.2021 )/src/app.ts
@@ -1,18 +1,74 @@
 export class App {
     Main(){
         this.createInput();
-        this.createWeatherBox();
-        this.createWeatherBox();
+        this.searchInput();
+        this.createWeatherBox('KATOWICE');
+        this.createWeatherBox('RADOM');
     }
     createInput(){
         const input: HTMLInputElement = document.createElement("input");
-        document.body.appendChild(input)
+        input.className = "textInput";
+        input.id = "textInput";
+        document.body.appendChild(input)   
     }
-    createWeatherBox(){
+    searchInput(){
+        const searchInput: HTMLInputElement = document.createElement("input");
+        searchInput.className = "searchInput";
+        searchInput.value = "+";
+        searchInput.addEventListener("click",getTownName);
+        document.body.appendChild(searchInput);
+        function getTownName(){
+            var townValue = document.getElementById("textInput");
+            console.log(townValue.value)
+        }
+    }
+    createWeatherBox(dataName:string){
+        let data:any = localStorage.getItem(dataName);
+        data = JSON.parse(data)
+
         const divBox: HTMLElement = document.createElement("div");
-        divBox.innerHTML = "KLIKER";
+        const temperature: HTMLElement = document.createElement("div");
+        const clouds: HTMLElement = document.createElement("div");
+        const info: HTMLElement = document.createElement("div");
+        const moreinfo: HTMLElement = document.createElement("div");
         divBox.className = "divBox";
-        document.body.appendChild(divBox)
+        temperature.className = "temperature";
+        clouds.className = "clouds";
+        info.className = "info";
+        moreinfo.className = "moreinfo";
+
+        divBox.innerHTML = data.name;
+        temperature.innerHTML = `${Math.floor(data.main.temp)} °C`;
+        clouds.innerHTML = data.weather[0].main;
+        info.innerHTML = `MORE INFO`;
+        info.addEventListener("click",showMore);
+        moreinfo.innerHTML = `
+        <ul>
+         <li> feels like: ${data.main.feels_like}</li> 
+         <li> temp min: ${data.main.temp_min}</li> 
+         <li> temp max: ${data.main.temp_max}</li> 
+         <li> wind speed: ${data.wind.speed}</li>    
+        </ul>
+        `;
+
+        divBox.appendChild(temperature);
+        divBox.appendChild(clouds);
+        divBox.appendChild(info);
+        divBox.appendChild(moreinfo);
+        document.body.appendChild(divBox);
+
+        function showMore(){
+            if(moreinfo.style.display !== "none"){
+                moreinfo.style.display = "none";
+            }else{
+                moreinfo.style.display = "block";
+            }
+            // if(moreinfo.className  !== "moreinfo"){
+            //     moreinfo.className = "moreinfo";
+            // }else{
+            //     moreinfo.className = "moreinfo-open";
+            // }
+        }
     }
     opwApiKey = '50d53005c0fd5f556bb4ef15224c4209';
     constructor(){
@@ -20,7 +76,9 @@ export class App {
     }
     async getCityInfo(city:string){
         const weather = await this.getWeather('radom');
-        this.saveData(weather);
+        const weather2 = await this.getWeather('katowice');
+        this.saveData("RADOM",weather);
+        this.saveData("KATOWICE",weather2);
     }
     async getWeather(city: string): Promise<any>{
         const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
@@ -29,17 +87,18 @@ export class App {
         console.log(weatherData);
         return weatherData;
     }
-    saveData(data:any){
-        localStorage.setItem('weatherData', JSON.stringify(data));
+    saveData(saveName:string ,data:any){
+        localStorage.setItem(saveName, JSON.stringify(data));
+
     }
-    getData(){
-        const data = localStorage.getItem('weatherData');
-        if (data){
-            return JSON.parse(data);
-        }else{
-            return {}
-        }
-    }
+    // getData(){
+    //     const data = localStorage.getItem('weatherData');
+    //     if (data){
+    //         return JSON.parse(data);
+    //     }else{
+    //         return {}
+    //     }
+    // }
 }
 
 
