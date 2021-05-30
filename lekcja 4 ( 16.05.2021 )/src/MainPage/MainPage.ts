b4f764d01c96205fa615c342cfe864e5f8dcf2b9
kaczormp4 <kaczkaoryginal@gmail.com>
2021-05-30 11:06:22 +0200

    obecny

--- lekcja 4 ( 16.05.2021 )/src/MainPage/MainPage.ts
+++ lekcja 4 ( 16.05.2021 )/src/MainPage/MainPage.ts
@@ -1,133 +1,211 @@
-import './MainPage.scss';
+import "./MainPage.scss";
+
+const avaibleColors = [
+  {
+    id: "lightsalmon",
+    color: "lightsalmon",
+  },
+  {
+    id: "teal",
+    color: "teal",
+  },
+  {
+    id: "lightblue",
+    color: "lightblue",
+  },
+  {
+    id: "yellow",
+    color: "yellow",
+  },
+  {
+    id: "green",
+    color: "green",
+  },
+  {
+    id: "lightgrey",
+    color: "lightgrey",
+  },
+];
+
+const dataStructure = [
+  {
+    id: 1,
+    tittle: "Tytuł",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when a",
+    color: "red",
+    archived: false,
+    favourite: false,
+    creationDate: "21-01-2020",
+  },
+  {
+    id: 2,
+    tittle: "Zakupy",
+    content: "Mleko, Pomidory, woda",
+    color: "yellow",
+    archived: false,
+    favourite: false,
+    creationDate: "21-01-2020",
+  },
+  {
+    id: 3,
+    tittle: "Rachunki",
+    content: "Prąd, Internet",
+    color: "green",
+    archived: false,
+    favourite: false,
+    creationDate: "21-01-2020",
+  },
+];
+
+const getData: any = JSON.parse(localStorage.getItem("myNotes"));
+
+const newDataStructure: {
+  id: string;
+  tittle: string;
+  content: string;
+  color: string;
+  archived: boolean;
+  favourite: boolean;
+  creationDate: string;
+}[] = [];
+
+newDataStructure.push(...getData);
 
 export class MainPage {
-    constructor(){
-        const getData = '';
-    }
-    Main(){
-        this.AddNote();
-        dataStructure.map(v => {
-            this.NoteBox(v.tittle, v.content, v.color ,v.archived, v.favourite);
-        })
-        
-    }
-    AddNote(){
-        const container: HTMLElement = document.createElement("div");
-        const inputTittle: HTMLInputElement = document.createElement("input");
-        const inputContent: HTMLTextAreaElement = document.createElement("textarea");
-        const colorPicker: HTMLInputElement = document.createElement("input");
-        const inputConfirm: HTMLButtonElement = document.createElement("button");
+  constructor() {}
+  Main() {
+    this.AddNote();
+    getData.map(
+      (v: {
+        id: string;
+        tittle: string;
+        content: string;
+        color: any;
+        archived: boolean;
+        favourite: boolean;
+      }) => {
+        this.NoteBox(v.id, v.tittle, v.content, v.color, v.archived, v.favourite);
+      }
+    );
+  }
+  AddNote() {
+    const container: HTMLElement = document.createElement("div");
+    const inputTittle: HTMLInputElement = document.createElement("input");
+    const inputContent: HTMLTextAreaElement =
+      document.createElement("textarea");
+    const inputConfirm: HTMLButtonElement = document.createElement("button");
 
-        let dataTab:any  = [];
+    let colorMemory = "";
+    container.className = "addNote";
+    inputTittle.className = "inputTittle";
+    inputContent.className = "inputContent";
+    inputConfirm.className = "inputConfirm";
 
-        colorPicker.type = "radio";
+    container.id = "addNote";
+    inputTittle.id = "inputTittle";
+    inputContent.id = "inputContent";
+    inputConfirm.id = "inputConfirm";
 
-        container.className = "addNote";
-        inputTittle.className = "inputTittle";
-        inputContent.className = "inputContent";
-        colorPicker.className = "colorPicker";
-        inputConfirm.className = "inputConfirm";
+    inputTittle.placeholder = "Create Tittle here..";
+    inputContent.placeholder = "Create note here..";
+    inputConfirm.innerHTML = "ADD NOTE";
 
-        container.id = "addNote";
-        inputTittle.id = "inputTittle";
-        inputContent.id = "inputContent";
-        colorPicker.id = "colorPicker";
-        inputConfirm.id = "inputConfirm";
+    document.getElementById("globalAddNote").appendChild(container);
+    container.appendChild(inputTittle);
+    container.appendChild(inputContent);
 
-        inputTittle.placeholder = "Create Tittle here..";
-        inputContent.placeholder = "Create note here..";
+    avaibleColors.map((d) => {
+      const colorPicker: HTMLElement = document.createElement("div");
+      colorPicker.className = "colorPicker";
+      colorPicker.id = d.id;
+      colorPicker.style.backgroundColor = d.color;
+      container.appendChild(colorPicker);
 
+      colorPicker.addEventListener("click", function () {
+        colorMemory = colorPicker.id;
+        inputConfirm.style.color = colorPicker.id;
+        inputConfirm.style.border = `1px solid ${colorPicker.id}`;
+      });
+    });
+    container.appendChild(inputConfirm);
 
-        document.getElementById('globalAddNote').appendChild(container);   
-        container.appendChild(inputTittle);   
-        container.appendChild(inputContent);   
-        container.appendChild(colorPicker);     
-        container.appendChild(inputConfirm);   
+    inputConfirm.addEventListener("click", function () {
+      newDataStructure.push({
+        id: `id_${inputTittle.value}_${colorMemory}_${Math.floor(Math.random() * 100)}`,
+        tittle: inputTittle.value,
+        content: inputContent.value,
+        color: colorMemory,
+        archived: false,
+        favourite: false,
+        creationDate: "22-22-22",
+      });
+      localStorage.setItem("myNotes", JSON.stringify(newDataStructure));
+      window.location.reload();
+    });
+  }
+  NoteBox(
+    id: string,
+    tittle: string,
+    contentTxt: string,
+    color: any,
+    archived: boolean,
+    favourite: boolean
+  ) {
+    const container: HTMLElement = document.createElement("div");
+    const titleTools: HTMLElement = document.createElement("div");
+    const isFav: HTMLElement = document.createElement("div");
+    const content: HTMLElement = document.createElement("div");
 
-        inputConfirm.addEventListener("click",function(){
-            dataStructure.push(
-                {
-                    id: 1,
-                    tittle: inputTittle.value,
-                    content: inputContent.value,
-                    color: 'purple',
-                    archived: false,
-                    favourite: false,
-                    creationDate: '21-01-2020',
+    container.className = "noteBox";
+    titleTools.className = "titleTools";
+    isFav.className = "isFav";
+    content.className = "noteBoxContent";
+
+    container.id = id;
+    titleTools.id = "titleTools";
+    isFav.id = "isFav";
+    content.id = "content";
+
+    container.style.backgroundColor = color;
+    container.style.borderColor = color;
+
+    titleTools.innerHTML = tittle;
+    isFav.innerHTML = '★';
+    content.innerHTML = contentTxt;
+
+    if( favourite ){
+        document.getElementById("globalFavourite").appendChild(container);
+        isFav.className = "isFav";
+        isFav.addEventListener("click", function () {
+            newDataStructure.map(v =>{
+                if(v.id === container.id){
+                    v.favourite = !v.favourite;
+                    localStorage.setItem("myNotes", JSON.stringify(newDataStructure));
                 }
-            )
-            console.log(dataStructure)
-        })
-    }
-    NoteBox(tittle: string, contentTxt: string, color: any, archived: boolean, favourite: boolean){
-        const container: HTMLElement = document.createElement("div");
-        const titleTools: HTMLElement = document.createElement("div");
-        const content: HTMLElement = document.createElement("div");
-
-        container.className = "noteBox";
-        titleTools.className = "titleTools";
-        content.className = "noteBoxContent";
-
-        container.id = "container";
-        titleTools.id = "titleTools";
-        content.id = "content";
-
-        container.style.backgroundColor = color;
-        container.style.borderColor = color;
-
-        titleTools.innerHTML = tittle;
-        content.innerHTML = contentTxt;
-
-        document.getElementById('globalNotes').appendChild(container);  
+            })
+            console.log(newDataStructure)
+          });
+        container.appendChild(isFav);
         container.appendChild(titleTools);
         container.appendChild(content);
     }
-}
+    else{
+        document.getElementById("globalNotes").appendChild(container);
+        isFav.className = "isNoFav";
+        container.appendChild(isFav);
+        container.appendChild(titleTools);
+        container.appendChild(content);
+    }
+  }
 
-const dataStructure = [
-    {
-        id: 1,
-        tittle: 'Tytuł',
-        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when a',
-        color: 'red',
-        archived: false,
-        favourite: false,
-        creationDate: '21-01-2020',
-    },
-    {
-        id: 2,
-        tittle: 'Zakupy',
-        content: 'Mleko, Pomidory, woda',
-        color: 'yellow',
-        archived: false,
-        favourite: false,
-        creationDate: '21-01-2020',
-    },
-    {
-        id: 3,
-        tittle: 'Rachunki',
-        content: 'Prąd, Internet',
-        color: 'green',
-        archived: false,
-        favourite: false,
-        creationDate: '21-01-2020',
-    },
-    {
-        id: 4,
-        tittle: 'Plany',
-        content: 'Dom, Ferrari',
-        color: 'grey',
-        archived: false,
-        favourite: false,
-        creationDate: '21-01-2020',
-    },
-    {
-        id: 5,
-        tittle: 'Drzewa',
-        content: 'Dąb, Lipa',
-        color: 'lightgreen',
-        archived: false,
-        favourite: false,
-        creationDate: '21-01-2020',
-    },
-]
+  getDateToday() {
+    let today: any = new Date();
+    let dd = String(today.getDate()).padStart(2, "0");
+    let mm = String(today.getMonth() + 1).padStart(2, "0");
+    let yyyy = today.getFullYear();
+
+    today = mm + "/" + dd + "/" + yyyy;
+    return today;
+  }
+}
